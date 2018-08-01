d6b880aba468abbcc1eaf5908776a688deac9488
asus-wolfcode-beetle <775121173@qq.com>
2018-08-01 20:44:25 +0800

    添加存储类

---
+++ fdfs/storage.py
@@ -0,0 +1,27 @@
+from django.core.files.storage import Storage
+from fdfs_client.client import Fdfs_client
+
+#尝试改写django的存储类
+#其实,我自己想基本的存储类的模式是什么都不知道.哈哈哈.
+class FDFSStorage(Storage):
+    '''fast dfs文件存储类'''
+
+    def _open(self,name,model='rb'):
+        pass
+    
+    def _save(self,name,content):
+        '''保存文件内容'''
+        #content包含你上传到fdfs
+        #name上传时候的名字
+        #创建一个file类型对象
+        client = Fdfs_client('/etc/fdfs/client.conf')
+        #上传到fdfs系统中
+        res = client.upload_by_buffer(content.read())
+
+        if res.get('Status') != 'Upload successed.':
+            raise Exception("上传文件到fast dfs失败!")
+
+        #获取返回的文件ID
+        filename = res.get('Remote file_id')
+
+        return filename
