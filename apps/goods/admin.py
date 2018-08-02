e9fd45ca9a7225ed7a463f1e1621f6770bd82144
asus-wolfcode-beetle <775121173@qq.com>
2018-08-02 17:03:11 +0800

    重构到一个父类里了.

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from goods.models import GoodsType,IndexPromotionBanner
+from goods.models import GoodsType,IndexPromotionBanner,IndexGoodsBanner,IndexTypeGoodsBanner
 # Register your models here.
 
 #admin.site.register(GoodsType)
 
-#写新类
-class IndexPromotionBannerAdmin(admin.ModelAdmin):
+class BaseModelAdmin(admin.ModelAdmin):
+
     def save_model(self,request,obj,form,change):
         '''新增或者更新表仲的数据时调用'''
         super().save_model(request,obj,form,change)
@@ -20,5 +20,22 @@ class IndexPromotionBannerAdmin(admin.ModelAdmin):
         from celery_tasks.tasks import generate_static_index_html
         generate_static_index_html.delay()
 
-admin.site.register(GoodsType)
+#写新类
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+    
+
+
 admin.site.register(IndexPromotionBanner,IndexPromotionBannerAdmin)
+admin.site.register(GoodsType,GoodsTypeAdmin)
+admin.site.register(IndexGoodsBanner,IndexGoodsBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner,IndexTypeGoodsBannerAdmin)
