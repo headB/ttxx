274f6e312ee6aaad322efb5426dfd96121276bbf
asus-wolfcode-beetle <775121173@qq.com>
2018-08-10 10:19:09 +0800

    完成了购物车页面显示的增加,减少,还有删除功能了.

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -5,7 +5,8 @@ app_name = 'cart'
 urlpatterns=[
     path('add',views.CartAddView.as_view(),name='add'),#购物车商品添加
     path('',views.CartInfoView.as_view(),name='show'),#购物车页面显示
-    path('update',views.CartUpdateView.as_view(),name='update'),#购物车页面显示
+    path('update',views.CartUpdateView.as_view(),name='update'),#购物车页面更新
+    path('delete',views.CartDeleteView.as_view(),name='delete'),#购物车记录删除
 
 
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -50,7 +50,7 @@ class CartAddView(View):
         cart_key = 'cart_%d'%user.id
 
         #尝试获取sku_id的值
-        cart_count = int(conn.hget(cart_key,sku_id))
+        cart_count = conn.hget(cart_key,sku_id)
         print(cart_count)
         if cart_count:
             #如果存在就进行累加
@@ -183,4 +183,44 @@ class CartUpdateView(View):
         
         #返回应答
         return JsonResponse({'res':5,'message':'更新成功!','total_count':total_count})
+
+
+class CartDeleteView(View):
+    '''购物车记录删除'''
+    def post(self, request):
+        '''购物车记录删除'''
+        user = request.user
+        if not user.is_authenticated:
+            # 用户未登录
+            return JsonResponse({'res': 0, 'errmsg': '请先登录'})
+
+        # 接收参数
+        sku_id = request.POST.get('sku_id')
+
+        # 数据的校验
+        if not sku_id:
+            return JsonResponse({'res':1, 'errmsg':'无效的商品id'})
+
+        # 校验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res':2, 'errmsg':'商品不存在'})
+
+        # 业务处理:删除购物车记录
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d'%user.id
+
+        # 删除 hdel
+        conn.hdel(cart_key, sku_id)
+
+        # 计算用户购物车中商品的总件数 {'1':5, '2':3}
+        total_count = 0
+        vals = conn.hvals(cart_key)
+        for val in vals:
+            total_count += int(val)
+
+        # 返回应答
+        return JsonResponse({'res':3, 'total_count':total_count, 'message':'删除成功'})
 
