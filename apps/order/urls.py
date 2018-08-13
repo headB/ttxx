c2a515588ed78cf2885ebc90717d677799fdf64e
asus-wolfcode-beetle <775121173@qq.com>
2018-08-13 10:41:19 +0800

    算是完结了.!支付搞掂了,可以自动监测支付情况,可以评论.

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('place',views.OrderPlaceView.as_view(),name='place'),
     path('commit',views.OrderCommitView.as_view(),name='commit'),
     path('pay', views.OrderPayView.as_view(), name='pay'), # 订单支付
+    path('check', views.CheckPayView.as_view(), name='check'), # 订单支付
+    path('comment/<int:order_id>', views.CommentView.as_view(), name='comment'), # 订单支付
 ]
